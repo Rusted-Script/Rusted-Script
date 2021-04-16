dc244042e68c52e6770a346c142dc04d11560dec
CertifiedRice <73188873+CertifiedRice@users.noreply.github.com>
2021-04-16 17:45:43 -0400

    Update var_access_node.py

--- src/var_access_node.py
+++ src/var_access_node.py
@@ -1,3 +1,9 @@
+# Copyright (c) Rusted Studio
+# Licensed under APGL-3.0 license. Read LiCENSE.txt for more info
+#Developers:
+# CertifiedRice - Lead Developer
+# Rusted Studio - Development Studio
+
 class varAccessNode:
     def __init__(self, var_name_tok):
         self.var_name_tok = var_name_tok
