77557c6209cdcb7abb5e00d1727bf86dbdd92189
CertifiedRice <73188873+CertifiedRice@users.noreply.github.com>
2021-04-16 17:45:12 -0400

    Update symbol_table.py

--- src/symbol_table.py
+++ src/symbol_table.py
@@ -1,3 +1,9 @@
+# Copyright (c) Rusted Studio
+# Licensed under APGL-3.0 license. Read LiCENSE.txt for more info
+#Developers:
+# CertifiedRice - Lead Developer
+# Rusted Studio - Development Studio
+
 class SymbolTable:
     def __init__(self):
         self.symbols = {}
