6a6f863854a8e6576e4132c5a339dc07b2af328e
CertifiedRice <73188873+CertifiedRice@users.noreply.github.com>
2021-04-16 17:43:10 -0400

    Update context.py

--- src/context.py
+++ src/context.py
@@ -1,3 +1,9 @@
+# Copyright (c) Rusted Studio
+# Licensed under APGL-3.0 license. Read LiCENSE.txt for more info
+#Developers:
+# CertifiedRice - Lead Developer
+# Rusted Studio - Development Studio
+
 class Context:
     def __init__(self, display_name, parent=None, parent_entry_pos=None):
         self.display_name = display_name
