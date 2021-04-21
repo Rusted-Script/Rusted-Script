7927642ff4f1a234d694a4e435527ce98a577af3
glaukiol1 <glaukiollupo372@gmail.com>
2021-04-21 18:59:59 +0000

    Updated

--- src/expected_char_error.py
+++ src/expected_char_error.py
@@ -5,3 +5,7 @@
 # Rusted Studio - Development Studio
 
 from rustedscript import *
+
+class ExpectedCharError(Error):
+    def __init__(self, pos_start, pos_end, details=''):
+        super().__init__(pos_start, pos_end, 'Expected Character', details)
