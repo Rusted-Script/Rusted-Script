1ac0f6f947a8da7d621218054e28895a2f051407
glaukiol1 <glaukiollupo372@gmail.com>
2021-04-22 13:23:59 +0000

    Added support for comments

--- src/interpreter.py
+++ src/interpreter.py
@@ -6,15 +6,25 @@ with open(sys.argv[1]) as rustedfile: # open the rusted file
 	print("    Running {}".format(sys.argv[1])+'...\n')
 	lines = rustedfile.readlines()
 	for line in lines:
-		result, error = rustedscript.run('<stdin>', line[0:len(line)-1])
-		if error:
-			print(error.as_string())
-			print("\n---- \n FATAL: Process exited with non-zero exit code")
-			sys.exit(1)
-		elif result:
-			print(result)
-			print("\n---- \n SUCCESS: {} ran successfully".format(sys.argv[1]))
-			sys.exit(0)
+		indexOfComment = int( line.find("//") )
+		if indexOfComment == -1:
+			indexOfComment = int( line.find("#") )
+		if indexOfComment == -1:
+			indexOfComment = int( line.find("/*") )
+		if indexOfComment != -1:
+			newline=line[0:indexOfComment-1]
+		else:
+			newline=line
+		if newline:
+			result, error = rustedscript.run('<stdin>', newline[0:len(line)-1])
+			if error:
+				print(error.as_string())
+				print("\n---- \n FATAL: Process exited with non-zero exit code")
+				sys.exit(1)
+			elif result:
+				print(result)
+				print("\n---- \n SUCCESS: {} ran successfully".format(sys.argv[1]))
+				sys.exit(0)
 
 
 
