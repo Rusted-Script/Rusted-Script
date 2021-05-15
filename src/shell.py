e0ce7263b4efd50eea892b3bb101d570b065688a
CertifiedRice <73188873+CertifiedRice@users.noreply.github.com>
2021-05-15 09:39:40 -0400

    Update shell.py

--- src/shell.py
+++ src/shell.py
@@ -7,7 +7,7 @@
 import rustedscript
 
 while True:
-	text = input('Rusted_Script > ')
+	text = input('Rusted_Script >>> ')
 	result, error = rustedscript.run('<stdin>', text)
 
 	if error: print(error.as_string())
